21b140fa7283bf11c003d808a5df1bffbcedddeb
ziwen-web <jziwenchen@gmail.com>
2015-04-02 14:38:07 +0800

    push server files

---
+++ callback.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+include_once( 'config.php' );
+include_once( 'saetv2.ex.class.php' );
+
+$o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
+
+if (isset($_REQUEST['code'])) {
+	$keys = array();
+	$keys['code'] = $_REQUEST['code'];
+	$keys['redirect_uri'] = WB_CALLBACK_URL;
+	try {
+		$token = $o->getAccessToken( 'code', $keys ) ;
+	} catch (OAuthException $e) {
+	}
+}
+
+if ($token) {
+	$_SESSION['token'] = $token;
+	setcookie( 'weibojs_'.$o->client_id, http_build_query($token) );
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Diesel</title>
+<style>
+	body {background:url('bg.jpg') no-repeat;background-size:cover;text-align: center;padding-top:100px;}
+</style>
+</head>
+
+<body>
+    授权完成
+</body>
+</html>
+
+
+
+<?php
+} else {
+?>
+授权失败。
+<?php
+}
+?>
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '3137479599' );
+define( "WB_SKEY" , '879e6892c45bec72ced4cc0b8962f06a' );
+define( "WB_CALLBACK_URL" , 'http://sfw15.dieselchina.com.cn/callback.php' );
